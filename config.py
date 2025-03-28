e6b1dc656e0159fa2b61c68456b8212fbb52354a
guichag <160218486+guichag@users.noreply.github.com>
2025-03-28 12:00:43 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+### CONFIG FILE ###
+
+import os
+
+KSCALEPATH = "/gws/nopw/j04/kscale"
+KSCALEDATA = os.path.join(KSCALEPATH,"DATA")
+KSCALEDIR = os.path.join(DIRNAME,"KSCALE")
+KSCALEOUTPATH = os.path.join(KSCALEDIR,"outputs")
