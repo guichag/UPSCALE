de5534f058a89ccf83f55604c71d992c54d0548d
guichag <160218486+guichag@users.noreply.github.com>
2025-05-15 17:17:46 +0100

    Update read_data_catalog.py

--- read_data/read_data_catalog.py
+++ read_data/read_data_catalog.py
@@ -5,6 +5,8 @@ import os
 import intake
 import xarray as xr
 
+from easygems.healpix import attach_coords
+
 from config import KSCALEDATA, KSCALEOUTDIR
 from KSCALE.read_data.get_coords import get_nn_lon_lat_index
 
@@ -39,7 +41,7 @@ def get_ds_name_regional(region='CTC', resolution='km4p4', physics='RAL3P3', dri
 def get_dataset_global(resolution='n1280', driving='GAL9', zoom=7):
     cat = get_cat()
     ds_name = get_ds_name_global(resolution, driving)
-    out = cat[ds_name](time='PT1H', zoom=zoom).to_dask()
+    out = cat[ds_name](time='PT1H', zoom=zoom).to_dask() #.pipe(attach_coords)
 
     return out
 
@@ -47,7 +49,7 @@ def get_dataset_global(resolution='n1280', driving='GAL9', zoom=7):
 def get_dataset_regional(region='CTC', resolution='km4p4', physics='RAL3P3', driving='GAL9', reso_nest='n1280', zoom=7):
     cat = get_cat()
     ds_name = get_ds_name_regional(region, resolution, physics, driving, reso_nest)
-    out = cat[ds_name](time='PT1H', zoom=zoom).to_dask()
+    out = cat[ds_name](time='PT1H', zoom=zoom).to_dask()  #.pipe(attach_coords)
 
     return out
 
@@ -57,7 +59,15 @@ def get_2d_variable_global(resolution='n1280', driving='GAL9', zoom=7, variable=
     idx = get_nn_lon_lat_index(zoom, lat_range, lon_range)
     ds = get_dataset_global(resolution, driving, zoom)
     vardata = ds[variable]
-    out = vardata.isel(cell=idx)
+
+    lat_min = lat_range[0]
+    lat_max = lat_range[1]
+    lon_min = lon_range[0]
+    lon_max = lon_range[1]
+    assert lat_min < lat_max, "wrong latitude order"
+    assert lon_min < lon_max, "wrong longitude order"
+
+    out = vardata.sel(cell=idx)
 
     return out
 
@@ -67,19 +77,32 @@ def get_2d_variable_regional(region='CTC', resolution='km4p4', physics='RAL3P3',
     idx = get_nn_lon_lat_index(zoom, lat_range, lon_range)
     ds = get_dataset_regional(region, resolution, physics, driving, reso_nest, zoom)
     vardata = ds[variable]
-    out = vardata.isel(cell=idx)
+    out = vardata.sel(cell=idx)
 
     return out
 
 
-def load_daily_data_global(resolution='n1280', driving='GAL9', zoom=7, variable='hflsd', lat_range=(-30.,30), lon_range=(-180.,180.), year=2020):
+def load_data_global(resolution='n1280', driving='GAL9', zoom=7, variable='hflsd', lat_range=(-30.,30), lon_range=(-180.,180.), year=2020, timescale='1D'):
     lat_min = lat_range[0]
     lat_max = lat_range[1]
     lon_min = lon_range[0]
     lon_max = lon_range[1]
-    datapath = KSCALEOUTDIR + '/' + variable + '/' + driving + '/' + resolution + '/z' + str(zoom) + '/lat={0},{1}_lon={2},{3}'.format(lat_min, lat_max, lon_min, lon_max)
-    datafile = datapath + '/' + str(year) + '_daily.nc'
+    conf = 'um_glm_' + resolution + '_' + driving 
+    datapath = KSCALEOUTDIR + '/' + variable + '/' + conf + '/z' + str(zoom) + '/lat={0},{1}_lon={2},{3}'.format(lat_min, lat_max, lon_min, lon_max)
+    datafile = datapath + '/' + str(year) + '_' + timescale + '.nc'
     out = xr.open_dataarray(datafile)
 
     return out
 
+
+def load_data_regional(region='CTC', resolution='km4p4', physics='RAL3P3', driving='GAL9', reso_nest='n1280', zoom=7, variable='hflsd', lat_range=(-30.,30), lon_range=(-180.,180.), year=2020, timescale='1D'):
+    lat_min = lat_range[0]
+    lat_max = lat_range[1]
+    lon_min = lon_range[0]
+    lon_max = lon_range[1]
+    conf = 'um_' + region + '_' + resolution + '_' + physics + '_' + reso_nest + '_' + driving + '_nest'
+    datapath = KSCALEOUTDIR + '/' + variable + '/' + conf + '/z' + str(zoom) + '/lat={0},{1}_lon={2},{3}'.format(lat_min, lat_max, lon_min, lon_max)
+    datafile = datapath + '/' + str(year) + '_' + timescale + '.nc'
+    out = xr.open_dataarray(datafile)
+
+    return out
